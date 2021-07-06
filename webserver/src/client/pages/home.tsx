4e494e68b4e5de28cadf481bd3f6b593af5f6fd3
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2021-07-07 01:44:12 +0700

    Configure port

--- webserver/src/client/pages/home.tsx
+++ webserver/src/client/pages/home.tsx
@@ -3,14 +3,17 @@ import { NextPage } from 'next';
 import useSWR from 'swr';
 import fetch from 'unfetch'
 
+interface MessageResponse {
+  message: string;
+}
+
 const fetcher = (url: string) => fetch(url).then(r => r.json())
 const Home: NextPage = () => {
-  const { data } = useSWR('/api/message', fetcher);
-  console.log(data);
+  const { data } = useSWR<MessageResponse>('http://localhost:3000/api/message', fetcher);
   return data ? (
     <>
       <h1>Loaded from Nextjs</h1>
-      <h2>with message from api: {data}</h2>
+      <h2>message from gateway: {data.message}</h2>
     </>
   ) : <h1>Loading</h1>;
 };
